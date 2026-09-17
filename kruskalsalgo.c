e1b0fca001d7fcd4638ad3f42fefd67ffbc592e1
madhuhasini678-beep <madhuhasini678@gmail.com>
2026-09-17 16:36:29 +0530

    Add files via upload

---
+++ kruskalsalgo.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+int parent[10];
+
+int find(int x) {
+    while (parent[x] != x)
+        x = parent[x];
+
+    return x;
+}
+
+int main() {
+    int n, e, i, j;
+    int u[10], v[10], w[10];
+    int total = 0, count = 0;
+
+    printf("Vertices: ");
+    scanf("%d", &n);
+
+    printf("Edges: ");
+    scanf("%d", &e);
+
+    printf("Enter: start end cost\n");
+
+    for (i = 0; i < e; i++)
+        scanf("%d %d %d", &u[i], &v[i], &w[i]);
+
+    // Sort edges by cost
+    for (i = 0; i < e; i++) {
+        for (j = i + 1; j < e; j++) {
+            if (w[i] > w[j]) {
+                int temp;
+
+                temp = w[i]; w[i] = w[j]; w[j] = temp;
+                temp = u[i]; u[i] = u[j]; u[j] = temp;
+                temp = v[i]; v[i] = v[j]; v[j] = temp;
+            }
+        }
+    }
+
+    // Each vertex is its own group first
+    for (i = 0; i < n; i++)
+        parent[i] = i;
+
+    // Select minimum edges
+    for (i = 0; i < e && count < n - 1; i++) {
+        int a = find(u[i]);
+        int b = find(v[i]);
+
+        if (a != b) {
+            printf("%d - %d = %d\n", u[i], v[i], w[i]);
+
+            total = total + w[i];
+            parent[b] = a;
+            count++;
+        }
+    }
+
+    printf("Minimum cost = %d", total);
+
+    return 0;
+}
---
+++ primsalgo.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+#define INF 999
+
+int main() {
+    int n, a[10][10];
+    int visited[10] = {0};
+    int i, j, min, x, y, total = 0;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter cost matrix:\n");
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &a[i][j]);
+
+            if (a[i][j] == 0)
+                a[i][j] = INF;
+        }
+    }
+
+    visited[0] = 1;   // Start from vertex 0
+
+    for (int edge = 0; edge < n - 1; edge++) {
+        min = INF;
+
+        for (i = 0; i < n; i++) {
+            if (visited[i] == 1) {
+                for (j = 0; j < n; j++) {
+                    if (visited[j] == 0 && a[i][j] < min) {
+                        min = a[i][j];
+                        x = i;
+                        y = j;
+                    }
+                }
+            }
+        }
+
+        printf("%d - %d = %d\n", x, y, min);
+
+        visited[y] = 1;
+        total = total + min;
+    }
+
+    printf("Minimum cost = %d", total);
+
+    return 0;
+}
+/*0 10 6 5
+10 0 0 15
+6 0 0 4
+5 15 4 0*/
